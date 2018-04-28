258c69f0d9227eba1c784ab28f42d0fbf07d417b
m861997 <xb2m861997@gmail.com>
2018-04-29 00:30:08 +0800

    second pages

--- AboutUs/index.js
+++ AboutUs/index.js
@@ -24,5 +24,37 @@ $(document).ready(function(){
       top:"90%",
       left: "45%"
     },300)
+	$("#about_us").click(function(){
+		$("html, body").scrollTop(733);
+		$("#about_us_intro").show().animate({
+			top: "110%",
+			left: "7%"
+		},300)
+		$("#line").show(1300).animate({
+			top: "116%",
+			left: "7%"
+		},500)
+	})
+	$(window).on("scroll", function(){
+		if($(window).scrollTop()+$(window).height()>=1400){
+			$("#about_us_intro").show().animate({
+				top: "110%",
+				left: "7%"
+			},300)
+			$("#line").show(1300).animate({
+				top: "116%",
+				left: "7%"
+			},500)
+			$("#intro").show(1500).animate({
+				top: "120%",
+				left: "7%"
+			},300)
+			$("#icon").show(1800).animate({
+				top: "170%",
+				right: "18%"
+			},300)
+		};
+	})
 })
+
 
