cef757823bed37c6bda273279e9ae50208b62ac7
wangchangchun <j432985029@gmail.com>
2018-04-28 15:49:59 +0800

    first page finish(?)

--- AboutUs/index.js
+++ AboutUs/index.js
@@ -3,10 +3,26 @@ $(document).ready(function(){
     $('#pic2').show().animate({
       left:"3%",
       top:"18%"
-    },2000);
+    },1000);
     $('#pic1').show().animate({
       top:"3%",
       left: "18%"
-    },2000);
+    },1000);
+    $("#SRT_ch").show(1300).animate({
+      top:"10%",
+      left: "50%"
+    },300)
+    $("#SRT").show(1600).animate({
+      top:"25%",
+      left: "50%"
+    },300)
+    $("#lattice").show(1900).animate({
+      top:"50%",
+      left: "50%"
+    },300)
+    $("#about_us").show(2100).animate({
+      top:"90%",
+      left: "45%"
+    },300)
 })
 
