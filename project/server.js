9780d4717f0a0fa7037fe30dcea0873f306044d9
nelsonlai1 <E24056459@luffy.ee.ncku.edu.tw>
2018-05-13 04:46:02 +0800

    functional without password

--- project/server.js
+++ project/server.js
@@ -7,6 +7,27 @@ const fs = require('fs');
 
 app.listen(port);
 app.use(express.static(__dirname + '/public'));
+app.post("/sign_up_data",urlencodedParser,function(req,res){
+	if(req.body.ID==""||req.body.PASSWORD==""||req.body.NAME==""){
+    res.send('please fill all blanks');
+    }
+    else{
+		fs.readFile('name.json',{},function(err,data){
+    var user;
+    //var id,password,name;
+		if(err){throw err;}
+		user=JSON.parse(data);
+    if(req.body.ID==user[req.body.NAME])
+      res.send('user already exist!');
+		if(!user.hasOwnProperty(req.body.ID)){
+			user[req.body.ID]=req.body.NAME;
+      //user[req.body.NAME]=req.body.PASSWORD;
+			fs.writeFile("name.json",JSON.stringify(user),function(err){if(err)throw err;});
+			res.send('user saved');
+			}
+		});
+	}
+});
 app.post("/login_data",urlencodedParser,function(req, res) {
 	if(req.body.ID==""||req.body.PASSWORD==""){
     res.send('please fill all blanks');
@@ -18,29 +39,12 @@ app.post("/login_data",urlencodedParser,function(req, res) {
 		if(!user.hasOwnProperty(req.body.ID)){
 			res.send('user not exist!');
 		}
-		else if(!user.hasOwnProperty(req.body.PASSWORD)){
+		/*else if(!user.hasOwnProperty(req.body.PASSWORD)){
 			res.send('Password is wrong');
-		}
+		}*/
 		else{
 			res.send('Hello,'+user[req.body.ID]);
 		}
-		});
-	}
-});
-app.post("/sign_up_result",urlencodedParser,function(req,res){
-	if(req.body.ID==""||req.body.PASSWORD==""||req.body.NAME==""){
-    res.send('please fill all blanks');
-    }
-    else{
-		fs.readFile('name.json',{},function(err,data){
-		var user;
-		if(err){throw err;}
-		user=JSON.parse(data);
-		if(!user.hasOwnProperty(req.body.ID)){
-			user[req.body.ID]=[req.body.PASSWORD:req.body.NAME];
-			fs.writeFile("name.json",JSON.stringify(user),functtion(err){if(err)throw err;});
-			res.send('user saved');
-			}
 		});
 	}
 });
